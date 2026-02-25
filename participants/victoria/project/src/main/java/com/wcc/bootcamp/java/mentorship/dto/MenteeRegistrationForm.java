92c7f9c856624fdeac37c7057473cb7933dd0eaf
victoria.holland <victoria.holland@verisk.com>
2026-02-25 15:28:17 +0000

    Improved security

--- participants/victoria/project/src/main/java/com/wcc/bootcamp/java/mentorship/dto/MenteeRegistrationForm.java
+++ participants/victoria/project/src/main/java/com/wcc/bootcamp/java/mentorship/dto/MenteeRegistrationForm.java
@@ -9,13 +9,17 @@ public class MenteeRegistrationForm {
 
     @NotBlank(message = "Name is required")
     @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    @Pattern(regexp = "^[\\p{L}\\p{M}\\s.'-]+$", message = "Name contains invalid characters")
     private String name;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Please provide a valid email address")
+    @Size(max = 255, message = "Email must not exceed 255 characters")
     private String email;
 
     @NotBlank(message = "At least one learning goal is required")
+    @Size(max = 1000, message = "Learning goals must not exceed 1000 characters")
+    @Pattern(regexp = "^[\\p{L}\\p{N}\\s,._#+-]+$", message = "Learning goals contain invalid characters")
     private String learningGoals;
 
     @NotBlank(message = "Experience level is required")
--- participants/victoria/project/src/main/java/com/wcc/bootcamp/java/mentorship/dto/MentorRegistrationForm.java
+++ participants/victoria/project/src/main/java/com/wcc/bootcamp/java/mentorship/dto/MentorRegistrationForm.java
@@ -9,13 +9,17 @@ public class MentorRegistrationForm {
 
     @NotBlank(message = "Name is required")
     @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    @Pattern(regexp = "^[\\p{L}\\p{M}\\s.'-]+$", message = "Name contains invalid characters")
     private String name;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Please provide a valid email address")
+    @Size(max = 255, message = "Email must not exceed 255 characters")
     private String email;
 
     @NotBlank(message = "At least one skill/expertise area is required")
+    @Size(max = 1000, message = "Skills must not exceed 1000 characters")
+    @Pattern(regexp = "^[\\p{L}\\p{N}\\s,._#+-]+$", message = "Skills contain invalid characters")
     private String skills;
 
     @Min(value = 1, message = "Must accept at least 1 mentee")
